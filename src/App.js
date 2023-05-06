87b41f7043c23133b0b1c84b423f81f8897f6ee2
AfiNaufal97L <naufal.setiawan92@gmail.com>
2023-05-06 23:39:41 +0700

    movie

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input placeholder="cari judul film" className="Movie-search"/>
+        <div className="Movie-container">
+          <div className="Movie-wrapper">
+            <div className="Movie-title">Title 1</div>
+            <img className="Movie-image" />
+            <div className="Movie-date">11-12-2022</div>
+            <div className="Movie-rate">8.9</div>
+          </div>
+        </div>
+
       </header>
     </div>
   );
